daa346ba80d66f124b0fd583b13936ba2c680aa4
subirdas29 <subirdas1045@gmail.com>
2024-09-23 00:05:09 +0600

    nav edited

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import logo from '../../assets/img/footer/logo.svg'
+import logo2 from '../../assets/img/nav/fav.png'
 
 const Navbar = () => {
 
    
   return (
     <div>
-      <div className="navbar bg-base-100 border">
+      <div className="navbar bg-base-100 fixed z-50 shadow-lg px-8 lg:px-32">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -25,42 +27,42 @@ const Navbar = () => {
       {/* //mobile formate */}
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow ">
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] top-10 mt-3 w-60 p-2 shadow ">
            
-           <li><a>Catalog</a></li>
+           <li className='mt-3'><a>Catalog</a></li>
       <li >
         <details>
-          <summary className='dropdown dropdown-hover'>How it works</summary>
+          <summary className='dropdown dropdown-hover my-3'>How it works</summary>
           <ul className=" rounded-none ">
-            <li className='hover:text-blue-500'><a>How Printify Works</a></li>
-            <li className='hover:text-blue-500 '><a>Print On Demand</a></li>
-            <li className='hover:text-blue-500 '><a>Printify Quality Promise</a></li>
-            <li className='hover:text-blue-500 '><a>What to Sell?</a></li>
+            <li className='hover:text-[#29AB51] mt-3'><a>How Printify Works</a></li>
+            <li className='hover:text-[#29AB51]  my-3 '><a>Print On Demand</a></li>
+            <li className='hover:text-[#29AB51] '><a>Printify Quality Promise</a></li>
+            <li className='hover:text-[#29AB51] my-3 '><a>What to Sell?</a></li>
           </ul>
         </details>
       </li>
       <li><a>Pricing</a></li>
-        <li><a>Blog</a></li>
+        <li className='my-3'><a>Blog</a></li>
         <li>
           <details>
           <summary>Services</summary>
         <ul className="rounded-none ">
-            <li className='hover:text-blue-500'><a>Printify Studio</a></li>
-            <li className='hover:text-blue-500'><a>Printify Express Delivery</a></li>
-            <li className='hover:text-blue-500'><a>Transfer Products</a></li>
-            <li className='hover:text-blue-500'><a>Order In Bulk</a></li>
-            <li className='hover:text-blue-500'><a>Experts Program</a></li>
+            <li className='hover:text-[#29AB51] my-3'><a>Printify Studio</a></li>
+            <li className='hover:text-[#29AB51]'><a>Printify Express Delivery</a></li>
+            <li className='hover:text-[#29AB51] my-3'><a>Transfer Products</a></li>
+            <li className='hover:text-[#29AB51]'><a>Order In Bulk</a></li>
+            <li className='hover:text-[#29AB51] my-3'><a>Experts Program</a></li>
           </ul>
           </details>
         </li>
         <li>
           <details>
-          <summary>Use-cases</summary>
+          <summary className='my-3'>Use-cases</summary>
         <ul className="rounded-none">
-            <li className='hover:text-blue-500'><a>Merch For Fans</a></li>
-            <li className='hover:text-blue-500'><a>Merch For eCommerce</a></li>
-            <li className='hover:text-blue-500'><a>Merch For Enterprises</a></li>
-            <li className='hover:text-blue-500'><a>Grow Your Store</a></li>
+            <li className='hover:text-[#29AB51] mt-2 mb-3'><a>Merch For Fans</a></li>
+            <li className='hover:text-[#29AB51]'><a>Merch For eCommerce</a></li>
+            <li className='hover:text-[#29AB51] my-3'><a>Merch For Enterprises</a></li>
+            <li className='hover:text-[#29AB51] mb-3'><a>Grow Your Store</a></li>
           </ul>
           </details>
         </li>
@@ -69,17 +71,18 @@ const Navbar = () => {
           <details>
           <summary>Need-help?</summary>
         <ul className="rounded-none">
-            <li className='hover:text-blue-500'><a>Help Center</a></li>
-            <li className='hover:text-blue-500'><a>Contacts</a></li>
-            <li className='hover:text-blue-500'><a>My Requests</a></li>
+            <li className='hover:text-[#29AB51] mt-2'><a>Help Center</a></li>
+            <li className='hover:text-[#29AB51] my-3'><a>Contacts</a></li>
+            <li className='hover:text-[#29AB51] mb-3'><a>My Requests</a></li>
           </ul>
           </details>
         </li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">Printify</a>
+   <img src={logo} className='hidden md:block w-[135px] h-[50px]' alt="" />
+   <img src={logo2} className='block md:hidden' alt="" />
   </div>
-  <div className="navbar-center hidden lg:flex">
+  <div className="navbar-center hidden lg:flex ">
     
     {/* desktop formate */}
     <ul className="menu menu-horizontal px-1 ">
@@ -88,10 +91,10 @@ const Navbar = () => {
         <details>
           <summary className='dropdown dropdown-hover'>How it works</summary>
           <ul className="p-2 w-52 rounded-none border">
-            <li className='hover:text-blue-500'><a>How Printify Works</a></li>
-            <li className='hover:text-blue-500 '><a>Print On Demand</a></li>
-            <li className='hover:text-blue-500 '><a>Printify Quality Promise</a></li>
-            <li className='hover:text-blue-500 '><a>What to Sell?</a></li>
+            <li className='hover:text-[#29AB51]'><a>How Printify Works</a></li>
+            <li className='hover:text-[#29AB51] '><a>Print On Demand</a></li>
+            <li className='hover:text-[#29AB51] '><a>Printify Quality Promise</a></li>
+            <li className='hover:text-[#29AB51] '><a>What to Sell?</a></li>
           </ul>
         </details>
       </li>
@@ -101,11 +104,11 @@ const Navbar = () => {
           <details>
           <summary>Services</summary>
         <ul className="p-2 w-52 rounded-none border">
-            <li className='hover:text-blue-500'><a>Printify Studio</a></li>
-            <li className='hover:text-blue-500'><a>Printify Express Delivery</a></li>
-            <li className='hover:text-blue-500'><a>Transfer Products</a></li>
-            <li className='hover:text-blue-500'><a>Order In Bulk</a></li>
-            <li className='hover:text-blue-500'><a>Experts Program</a></li>
+            <li className='hover:text-[#29AB51]'><a>Printify Studio</a></li>
+            <li className='hover:text-[#29AB51]'><a>Printify Express Delivery</a></li>
+            <li className='hover:text-[#29AB51]'><a>Transfer Products</a></li>
+            <li className='hover:text-[#29AB51]'><a>Order In Bulk</a></li>
+            <li className='hover:text-[#29AB51]'><a>Experts Program</a></li>
           </ul>
           </details>
         </li>
@@ -113,10 +116,10 @@ const Navbar = () => {
           <details>
           <summary>Use-cases</summary>
         <ul className="p-2 w-52 rounded-none border">
-            <li className='hover:text-blue-500'><a>Merch For Fans</a></li>
-            <li className='hover:text-blue-500'><a>Merch For eCommerce</a></li>
-            <li className='hover:text-blue-500'><a>Merch For Enterprises</a></li>
-            <li className='hover:text-blue-500'><a>Grow Your Store</a></li>
+            <li className='hover:text-[#29AB51]'><a>Merch For Fans</a></li>
+            <li className='hover:text-[#29AB51]'><a>Merch For eCommerce</a></li>
+            <li className='hover:text-[#29AB51]'><a>Merch For Enterprises</a></li>
+            <li className='hover:text-[#29AB51]'><a>Grow Your Store</a></li>
           </ul>
           </details>
         </li>
@@ -125,9 +128,9 @@ const Navbar = () => {
           <details>
           <summary>Need-help?</summary>
         <ul className="p-2 w-52 rounded-none border">
-            <li className='hover:text-blue-500'><a>Help Center</a></li>
-            <li className='hover:text-blue-500'><a>Contacts</a></li>
-            <li className='hover:text-blue-500'><a>My Requests</a></li>
+            <li className='hover:text-[#29AB51]'><a>Help Center</a></li>
+            <li className='hover:text-[#29AB51]'><a>Contacts</a></li>
+            <li className='hover:text-[#29AB51]'><a>My Requests</a></li>
           </ul>
           </details>
         </li>
